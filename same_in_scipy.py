9e2187fbae02ce13e1160be18d64449b6bc17d41
alxfed <alxfed@gmail.com>
2018-08-15 10:43:43 -0500

    initial plot

--- same_in_scipy.py
+++ same_in_scipy.py
@@ -4,12 +4,18 @@ Same manipulations with the same data but in SciPy
 
 # First we input the same data manually
 
+import matplotlib.pyplot as plt
+
 t = [0, 1, 2, 3]
 x = [0.2, 0.4, 0.6, 0.8]
 y = [0.25, 0.55, 0.45, 0.7]
 
 a = 0.6; b = 0.2       # wild guess parameters of the linear model
 
+plt.axis('square')
+plt.axis([0, 1, 0, 1])
+plt.plot(x, y, 'ro')
 
+plt.show()
 
-print(t, x, y, a, b)
+# print(t, x, y, a, b)
