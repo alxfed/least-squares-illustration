b9e003dc0ce9a7c62ddca367941c5206b54dfc59
alxfed <alxfed@gmail.com>
2018-08-15 10:19:25 -0500

    py version

--- same_in_scipy.py
+++ same_in_scipy.py
@@ -4,3 +4,12 @@ Same manipulations with the same data but in SciPy
 
 # First we input the same data manually
 
+t = [0, 1, 2, 3]
+x = [0.2, 0.4, 0.6, 0.8]
+y = [0.25, 0.55, 0.45, 0.7]
+
+a = 0.6; b = 0.2       # wild guess parameters of the linear model
+
+
+
+print(t, x, y, a, b)
